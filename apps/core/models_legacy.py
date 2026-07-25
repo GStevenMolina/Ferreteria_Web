8031b2f76403fe746c56dd91d29186562c69b40d
stevenmoli23 <molinaavellangesslersteven@gmail.com>
2026-07-25 14:13:44 -0600

    subiendo correctamente la base de datos

--- apps/core/models_legacy.py
+++ apps/core/models_legacy.py
@@ -3,45 +3,45 @@
 #   * Rearrange models' order
 #   * Make sure each model has one field with primary_key=True
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+#   * Remove `managed = True` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
 class Beneficiocliente(models.Model):
     id_beneficio = models.AutoField(primary_key=True)
     id_cliente = models.ForeignKey('Cliente', models.DO_NOTHING, db_column='id_cliente', blank=True, null=True)
-    tipo = models.CharField(max_length=50, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
-    frecuencia = models.CharField(max_length=50, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
-    condicion = models.TextField(db_collation='Modern_Spanish_CI_AS', blank=True, null=True)  # This field type is a guess.
-    restricciones = models.TextField(db_collation='Modern_Spanish_CI_AS', blank=True, null=True)  # This field type is a guess.
+    tipo = models.CharField(max_length=50, blank=True, null=True)
+    frecuencia = models.CharField(max_length=50, blank=True, null=True)
+    condicion = models.TextField(blank=True, null=True)  # This field type is a guess.
+    restricciones = models.TextField(blank=True, null=True)  # This field type is a guess.
     porcentajedescuento = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'beneficiocliente'
 
 
 class Categoria(models.Model):
     id_categoria = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=100, db_collation='Modern_Spanish_CI_AS')
-    descripcion = models.TextField(db_collation='Modern_Spanish_CI_AS', blank=True, null=True)  # This field type is a guess.
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)  # This field type is a guess.
     fecha_creacion = models.DateTimeField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'categoria'
 
 
 class Cliente(models.Model):
     id_cliente = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=100, db_collation='Modern_Spanish_CI_AS')
-    telefono = models.CharField(max_length=20, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
-    direccion = models.TextField(db_collation='Modern_Spanish_CI_AS', blank=True, null=True)  # This field type is a guess.
+    nombre = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=20, blank=True, null=True)
+    direccion = models.TextField(blank=True, null=True)  # This field type is a guess.
     fecha_registro = models.DateTimeField(blank=True, null=True)
-    estado = models.CharField(max_length=10, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)  # <--- NUEVO
+    estado = models.CharField(max_length=10, blank=True, null=True)  # <--- NUEVO
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'cliente'
 
 
@@ -53,7 +53,7 @@ class Compra(models.Model):
     total = models.DecimalField(max_digits=18, decimal_places=2, blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'compra'
 
 
@@ -65,7 +65,7 @@ class DetalleCompra(models.Model):
     precio_unitario = models.DecimalField(max_digits=18, decimal_places=2, blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'detalle_compra'
 
 
@@ -78,7 +78,7 @@ class DetalleVenta(models.Model):
     subtotal = models.DecimalField(max_digits=18, decimal_places=2, blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'detalle_venta'
 
 
@@ -90,12 +90,12 @@ class Devolucion(models.Model):
     fecha = models.DateTimeField(blank=True, null=True)
     cantidad = models.IntegerField(default=1)
     plazo = models.IntegerField(blank=True, null=True)
-    condiciones = models.TextField(db_collation='Modern_Spanish_CI_AS', blank=True, null=True)  # This field type is a guess.
-    restricciones = models.TextField(db_collation='Modern_Spanish_CI_AS', blank=True, null=True)  # This field type is a guess.
-    estado = models.CharField(max_length=20, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
-    cantidad = models.IntegerField(default=1)
+    condiciones = models.TextField(blank=True, null=True)  # This field type is a guess.
+    restricciones = models.TextField(blank=True, null=True)  # This field type is a guess.
+    estado = models.CharField(max_length=20, blank=True, null=True)
+    
     class Meta:
-        managed = False
+        managed = True
         db_table = 'devolucion'
 
         # --- PROPIEDADES VIRTUALES (No tocan la Base de Datos) ---
@@ -127,32 +127,32 @@ class Devolucion(models.Model):
 class FacturaCliente(models.Model):
     id_factura = models.AutoField(primary_key=True)
     id_venta = models.ForeignKey('Venta', models.DO_NOTHING, db_column='id_venta', blank=True, null=True)
-    numero_factura = models.CharField(max_length=50, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
-    tipo_comprobante = models.CharField(max_length=50, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
+    numero_factura = models.CharField(max_length=50, blank=True, null=True)
+    tipo_comprobante = models.CharField(max_length=50, blank=True, null=True)
     fecha_emision = models.DateField(blank=True, null=True)
     subtotal = models.DecimalField(max_digits=18, decimal_places=2, blank=True, null=True)
     impuesto = models.DecimalField(max_digits=18, decimal_places=2, blank=True, null=True)
     total = models.DecimalField(max_digits=18, decimal_places=2, blank=True, null=True)
-    estado = models.CharField(max_length=20, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
+    estado = models.CharField(max_length=20, blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'factura_cliente'
 
 
 class FacturaProveedor(models.Model):
     id_factura_proveedor = models.AutoField(primary_key=True)
     id_compra = models.ForeignKey(Compra, models.DO_NOTHING, db_column='id_compra', blank=True, null=True)
-    numero_factura = models.CharField(max_length=50, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
-    tipo_comprobante = models.CharField(max_length=50, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
+    numero_factura = models.CharField(max_length=50, blank=True, null=True)
+    tipo_comprobante = models.CharField(max_length=50, blank=True, null=True)
     fecha_emision = models.DateField(blank=True, null=True)
     subtotal = models.DecimalField(max_digits=18, decimal_places=2, blank=True, null=True)
     impuesto = models.DecimalField(max_digits=18, decimal_places=2, blank=True, null=True)
     total = models.DecimalField(max_digits=18, decimal_places=2, blank=True, null=True)
-    estado = models.CharField(max_length=20, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
+    estado = models.CharField(max_length=20, blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'factura_proveedor'
 
 
@@ -165,7 +165,7 @@ class Inventario(models.Model):
     fecha_actualizacion = models.DateTimeField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'inventario'
 
 
@@ -173,14 +173,14 @@ class MovimientoInventario(models.Model):
     id_movimiento = models.AutoField(primary_key=True)
     id_producto = models.ForeignKey('Producto', models.DO_NOTHING, db_column='id_producto', blank=True, null=True)
     id_usuario = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='id_usuario', blank=True, null=True)
-    tipo_movimiento = models.CharField(max_length=50, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
+    tipo_movimiento = models.CharField(max_length=50, blank=True, null=True)
     cantidad = models.IntegerField(blank=True, null=True)
-    referencia = models.CharField(max_length=100, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
+    referencia = models.CharField(max_length=100, blank=True, null=True)
     fecha_movimiento = models.DateTimeField(blank=True, null=True)
-    observaciones = models.TextField(db_collation='Modern_Spanish_CI_AS', blank=True, null=True)  # This field type is a guess.
+    observaciones = models.TextField(blank=True, null=True)  # This field type is a guess.
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'movimiento_inventario'
 
 
@@ -188,47 +188,47 @@ class Producto(models.Model):
     id_producto = models.AutoField(primary_key=True)
     id_proveedor = models.ForeignKey('Proveedor', models.DO_NOTHING, db_column='id_proveedor', blank=True, null=True)
     id_categoria = models.ForeignKey(Categoria, models.DO_NOTHING, db_column='id_categoria', blank=True, null=True)
-    nombre = models.CharField(max_length=100, db_collation='Modern_Spanish_CI_AS')
-    descripcion = models.TextField(db_collation='Modern_Spanish_CI_AS', blank=True, null=True)  # This field type is a guess.
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)  # This field type is a guess.
     precio_compra = models.DecimalField(max_digits=18, decimal_places=2, blank=True, null=True)
     precio_venta = models.DecimalField(max_digits=18, decimal_places=2, blank=True, null=True)
-    unidad_medida = models.CharField(max_length=20, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
+    unidad_medida = models.CharField(max_length=20, blank=True, null=True)
     fecha_creacion = models.DateTimeField(blank=True, null=True)
     estado = models.BooleanField(default=True) # <--- NUEVO
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'producto'
 
 
 class Proveedor(models.Model):
     id_proveedor = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=100, db_collation='Modern_Spanish_CI_AS')
-    telefono = models.CharField(max_length=20, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
-    email = models.CharField(max_length=100, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
-    numero_contacto = models.CharField(max_length=100, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
-    direccion = models.TextField(db_collation='Modern_Spanish_CI_AS', blank=True, null=True)  # This field type is a guess.
-    tipo_proveedor = models.CharField(max_length=50, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
+    nombre = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=20, blank=True, null=True)
+    email = models.CharField(max_length=100, blank=True, null=True)
+    numero_contacto = models.CharField(max_length=100, blank=True, null=True)
+    direccion = models.TextField(blank=True, null=True)  # This field type is a guess.
+    tipo_proveedor = models.CharField(max_length=50, blank=True, null=True)
     fecha_registro = models.DateTimeField(blank=True, null=True)
-    estado = models.CharField(max_length=10, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)  # <--- NUEVO
+    estado = models.CharField(max_length=10, blank=True, null=True)  # <--- NUEVO
     
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'proveedor'
 
 
 class Usuario(models.Model):
     id_usuario = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=100, db_collation='Modern_Spanish_CI_AS')
-    email = models.CharField(unique=True, max_length=100, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
-    password = models.CharField(max_length=255, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
-    rol = models.CharField(max_length=50, db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
+    nombre = models.CharField(max_length=100)
+    email = models.CharField(unique=True, max_length=100, blank=True, null=True)
+    password = models.CharField(max_length=255, blank=True, null=True)
+    rol = models.CharField(max_length=50, blank=True, null=True)
     activo = models.BooleanField(blank=True, null=True)
     fecha_creacion = models.DateTimeField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'usuario'
 
 class Auditoria(models.Model):
@@ -243,14 +243,13 @@ class Auditoria(models.Model):
 
     class Meta:
         db_table = "auditoria"  # <--- Este nombre se usará en la base de datos
-        managed = False  # <--- Permite que Django administre esta tabla
+        managed = True  # <--- Permite que Django administre esta tabla
 
     def __str__(self):
         if self.usuario:
             return f"{self.fecha} - {self.usuario.nombre} - {'Éxito' if self.exito else 'Fallo'}"
         return f"{self.fecha} - {self.email} - {'Éxito' if self.exito else 'Fallo'}"
     
-    from django.db import models
 
 class AuditoriaEvento(models.Model):
     usuario = models.ForeignKey(
@@ -266,7 +265,7 @@ class AuditoriaEvento(models.Model):
 
     class Meta:
         db_table = "auditoria_evento"
-        managed = False  # Porque la creas tú desde SQL… Django NO la modifica.
+        managed = True  # Porque la creas tú desde SQL… Django NO la modifica.
 
     def __str__(self):
         return f"{self.fecha} - {self.evento} - {self.email}"
@@ -279,7 +278,7 @@ class Venta(models.Model):
     total = models.DecimalField(max_digits=18, decimal_places=2, blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'venta'
         
 class ProductoDanado(models.Model):
@@ -288,13 +287,13 @@ class ProductoDanado(models.Model):
     id_producto = models.ForeignKey('Producto', models.DO_NOTHING, db_column='id_producto')
     id_usuario = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='id_usuario', blank=True, null=True)
     cantidad = models.IntegerField(default=1)
-    motivo_dano = models.TextField(db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
-    estado_proceso = models.CharField(max_length=30, default='PENDIENTE', db_collation='Modern_Spanish_CI_AS')
+    motivo_dano = models.TextField(blank=True, null=True)
+    estado_proceso = models.CharField(max_length=30, default='PENDIENTE')
     fecha_registro = models.DateTimeField(auto_now_add=True)
-    observaciones = models.TextField(db_collation='Modern_Spanish_CI_AS', blank=True, null=True)
+    observaciones = models.TextField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'producto_danado'
 
     def __str__(self):
